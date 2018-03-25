2309b17f42940eb2e5d4ae1596a46baedec0ec7a
arechrisna <arechrisna@yahoo.com>
2018-03-25 14:44:40 +0100

    initial commit

---
+++ eclipse-setup/src/eclipse-setup.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : eclipse-setup.cpp
+// Author      : christian espinoza
+// Version     :
+// Copyright   : confidential and propietary
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "" << endl; // prints 
+	return 0;
+}
